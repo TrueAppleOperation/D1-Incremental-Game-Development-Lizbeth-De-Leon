4b024553c7b3d790c5f92f27bec5199e0f6377d2
Fishbowl <icecoffee1945@gmail.com>
2025-10-08 22:56:04 +0000

    Added button click logic | Step One Complete!

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
+let counter: number = 0;
+
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <p>Counter: <span id="counter">0</span></p>
   <button id="increment">🌟</button>
 `;
 
 const button = document.getElementById("increment")!;
+const counterElement = document.getElementById("counter")!;
 
 button.addEventListener("click", () => {
+  counter++;
+  counterElement.textContent = counter.toString();
+  console.log("Counter-keeping:", button, counterElement, counter);
   console.log("Star button was clicked!");
 });
