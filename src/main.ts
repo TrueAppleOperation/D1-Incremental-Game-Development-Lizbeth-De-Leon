362cb9c3755b788270b7f9cc02d3c95154eeea21
Fishbowl <icecoffee1945@gmail.com>
2025-10-08 22:52:13 +0000

    Added button click logic | Step 1 Complete!

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,11 @@ import "./style.css";
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
+  <button id="increment">🌟</button>
 `;
+
+const button = document.getElementById("increment")!;
+
+button.addEventListener("click", () => {
+  console.log("Star button was clicked!");
+});
