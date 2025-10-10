5cffecec3120d43c353f5117fee989d6eeb99407
Fishbowl <icecoffee1945@gmail.com>
2025-10-10 10:29:32 +0000

    Added Continuous Growth (with requestAnimationFrame) | Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
 let counter: number = 0;
+let lastTime: number | null = null;
 
 document.body.innerHTML = `
   <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
@@ -19,9 +20,18 @@ button.addEventListener("click", () => {
   console.log("Star button was clicked!");
 });
 
-setInterval(autoClicker, 1000);
-
-function autoClicker() {
-  counter++;
-  counterElement.textContent = counter.toString();
+function autoClicker(timestamp: number) {
+  if (lastTime === null) {
+    lastTime = timestamp;
+  }
+  
+  const deltaTime = (timestamp - lastTime) / 1000; // increases based on real time
+  lastTime = timestamp;
+  
+  counter += deltaTime;
+  counterElement.textContent = counter.toFixed(2);
+  
+  requestAnimationFrame(autoClicker);
 }
+
+requestAnimationFrame(autoClicker); //starts animation loop
